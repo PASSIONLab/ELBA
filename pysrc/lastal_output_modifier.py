e2bc60444d69a289925a6389cdf31e00d183c13a
Saliya Ekanayake <esaliya@gmail.com>
2019-11-19 19:05:19 -0800

    * adding python scripts

---
+++ pysrc/lastal_output_modifier.py
@@ -0,0 +1,44 @@
+from Bio import SeqIO
+from pathlib import Path
+import csv
+import numpy as np
+import functools
+import operator
+import time
+
+
+def main():
+    overlaps_fname = '/Users/esaliya/sali/git/github/esaliya/cpp/lbl.pisa' \
+                     '/pysrc/data/cori/scope/last.shuff/filtered.gt30pid' \
+                     '.scope.77k.shuff.lastal.out.txt'
+
+    p = Path(overlaps_fname);
+
+    fixed_overlaps_fname = str(p.parent / Path("fixed_" + p.name))
+
+    seqs_fname = '/Users/esaliya/sali/data/scope/uniqs' \
+                 '/all/77040_unique_of_243813_astral-scopedom-seqres' \
+                 '-gd-all-2.07-stable.fa'
+
+    seq_id_2_idx = {}
+
+    with open(seqs_fname, "r") as seqf:
+        count = 0
+        for record in SeqIO.parse(seqf, "fasta"):
+            seq_id_2_idx[record.id] = count
+            count += 1
+
+    with open(overlaps_fname, 'rt') as csv_file, \
+            open(fixed_overlaps_fname, 'w', newline='') as fixed_csv:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        fixed_csv_writer = csv.writer(fixed_csv, delimiter=',')
+        fixed_csv_writer.writerow(['g_col_idx', 'g_row_idx', 'pid'])
+        for tup in csv_reader:
+            tup[0] = seq_id_2_idx[tup[0]]
+            tup[1] = seq_id_2_idx[tup[1]]
+            # fixed_csv_writer.writerow([tup[0], tup[1], tup[2]])
+            fixed_csv_writer.writerow(tup)
+
+
+if __name__ == '__main__':
+    main()
