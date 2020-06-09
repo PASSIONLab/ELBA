6bb1b48f940da666163e3f9859b36bfaa50eb908
Giulia Guidi <gl.guidi93@gmail.com>
2020-06-09 10:54:59 -0700

    build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+mkdir build_release
 cd build_release
+cmake ..
 make -j4
+cd ..
--- seqan.sh
+++ seqan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-wget https://github.com/seqan/seqan/releases/download/seqan-v2.4.0/seqan-library-2.4.0.zip
-unzip seqan-library-2.4.0
-rm seqan-library-2.4.0.zip
-export SEQAN_HOME=seqan-library-2.4.0/
+git clone --branch seqan-v2.4.0 https://github.com/seqan/seqan.git
+export SEQAN_HOME=$(pwd)/seqan/
 cd $SEQAN_HOME
 git checkout develop
