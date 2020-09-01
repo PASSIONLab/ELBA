5a0840c1f8d0cfd11c261529f3541db75ede8bf3
Giulia Guidi <gguidi@berkeley.edu>
2020-09-01 13:08:38 -0700

    updates from pastis to DistributedPairwiseRunner.hpp

--- include/DistributedPairwiseRunner.hpp
+++ include/DistributedPairwiseRunner.hpp
@@ -13,36 +13,30 @@
 #include "AlignmentInfo.hpp"
 #include "kmer/CommonKmers.hpp"
 
-//struct AlignmentInfo{
-//  seqan::AlignmentStats stats;
-//  ushort seq_h_length;
-//  ushort seq_v_length;
-//  ushort seq_h_seed_length;
-//  ushort seq_v_seed_length;
-//  uint64_t seq_h_g_idx;
-//  uint64_t seq_v_g_idx;
-//};
-
 class DistributedPairwiseRunner {
 public:
   DistributedPairwiseRunner(std::shared_ptr<DistributedFastaData> dfd,
-                     PSpMat<dibella::CommonKmers>::DCCols * localmat, int afreq,
+                     PSpMat<dibella::CommonKmers>::DCCols * localmat, 
+                     PSpMat<pastis::CommonKmers>::MPI_DCCols *glmat,
+                     int afreq,
 		                 uint64_t rowoffset, uint64_t coloffset,
                      const std::shared_ptr<ParallelOps> &parops);
 
-//  uint64_t align_seqs();
   void write_overlaps(const char *file);
   void run(PairwiseFunction *pf, const char* file, std::ofstream& lfs, int log_freq, ushort k);
-  void runv2(PairwiseFunction *pf, const char* file, std::ofstream& lfs, int log_freq, ushort k);
+  void run_batch(PairwiseFunction *pf, const std::string &aln_file,
+                    std::ofstream& lfs,
+                    int log_freq, int ckthr, float mosthr, TraceUtils tu,
+                    bool score_only = false, ushort k);
 
 private:
   PSpMat<dibella::CommonKmers>::DCCols * spSeq;
+  PSpMat<pastis::CommonKmers>::MPI_DCCols * gmat;
   uint64_t row_offset;  // local to global row id offset  
   uint64_t col_offset;	// ditto
   std::shared_ptr<DistributedFastaData> dfd;
   int afreq;
   std::shared_ptr<ParallelOps> parops;
 };
-
 
 #endif //LBL_DAL_ALIGNER_H
