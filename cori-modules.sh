771d3a329e236e5a7e1acb158049cb986b5ac56d
Giulia Guidi <gl.guidi93@gmail.com>
2020-09-14 16:32:51 -0700

    fix

--- cori-modules.sh
+++ cori-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 module swap PrgEnv-intel PrgEnv-gnu && module load cmake && module load boost && module load python
-export COMBBLAS_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/CombBLAS
+export COMBBLAS_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/
 export BLOOM_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/src/libbloom/
 export SEQAN_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/seqan
