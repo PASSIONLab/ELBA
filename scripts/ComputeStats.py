480117f8ab089588e33e365a31c4222ee83c22fd
Giulia Guidi <gl.guidi93@gmail.com>
2020-09-15 15:44:38 -0700

    minor fixes

--- scripts/ComputeStats.py
+++ scripts/ComputeStats.py
@@ -9,12 +9,12 @@ import numpy as np
  
 LogFileName = str(sys.argv[1])
 ExportFile  = "/global/cscratch1/sd/gguidi/IPDPS2021/performance/"
-ExportFile  = ExportFile + "ComputeStats" + LogFileName + ".txt" 
+ExportFile  = ExportFile + "ComputeStats-" + LogFileName + ".txt" 
 
 sums = np.zeros(12)
 
 # @GGGG: run this within a folder with the files I'm interested in averaging
-ListOfFiles = glob.glob('*GetTime*.txt') # Create the list of file that I already parsed
+ListOfFiles = glob.glob('GetTime-' + LogFileName + '*.txt') # Create the list of file that I already parsed
 for ThisFile in ListOfFiles:
   FI = open(ThisFile, 'r')
   numbersFromFile = [float(line) for line in FI.readlines()]
@@ -29,5 +29,5 @@ with open(ExportFile, "w+") as file:
     file.write(LogFileName + "\n")  # First line is gonna be a header
     for i in xrange(0, len(sums)):
         print sums[i]
-        file.write(sums[i] + "\n")
+        file.write(str(sums[i]) + "\n")
 file.close()
--- scripts/GetTime.py
+++ scripts/GetTime.py
@@ -10,7 +10,7 @@ LogFileName = open(sys.argv[1], 'r')
 ExportFile = "/global/cscratch1/sd/gguidi/IPDPS2021/performance/"
 
 # @GGGG: run the program from build folder (temporary)
-ExportFile = ExportFile + "GetTime" + str(sys.argv[1]) + ".txt"
+ExportFile = ExportFile + "GetTime-" + str(sys.argv[1]) + ".txt"
 
 regex1st = '(Main|\(\):[0-9]).*' # Only care about the last lines of the log
 regex2nd = '[0-9].*[0-9]'        # Extract only numeric values 
@@ -25,15 +25,17 @@ with open(sys.argv[1], "r") as file:
             for num in re.finditer(regex2nd, MatchText, re.S):
                 MatchText2 = num.group()
                 MatchList.append(MatchText2)
-                print MatchText2
+                # print MatchText2
 file.close()
  
 with open(ExportFile, "w+") as file:
     for item in [x for x in xrange(0, len(MatchList)) if x != 2 and x != 3 and x != 8]: # Remove lines I'm not interested in
-        if ToSecond = True:
+        if ToSecond == True:
             ms = float(MatchList[item]) # Trasform from ms to s
             s  = ms / 1000
+            print str(s)
             file.write(str(s) + "\n")
         else:
+            print MatchList[item]
             file.write(MatchList[item] + "\n")
 file.close()
