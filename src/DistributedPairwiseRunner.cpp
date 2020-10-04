eae35bb440d10c651ccecc5df08a8dac0cd2d425
Giulia Guidi <gguidi@batch4.summit.olcf.ornl.gov>
2020-10-04 16:49:11 -0400

    trying to fix a bug in distributed alignment and testing my patience

--- src/DistributedPairwiseRunner.cpp
+++ src/DistributedPairwiseRunner.cpp
@@ -215,10 +215,10 @@ DistributedPairwiseRunner::run_batch
 	int myrank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-	int			batch_size		= 1e5;
+	int			batch_size		= 1e4;
 	int			batch_cnt		= (local_nnz_count / batch_size) + 1;
 	int			batch_idx		= 0;
-	uint64_t	nalignments		= 0;
+	uint64_t		nalignments		= 0;
 
 	// PSpMat<dibella::CommonKmers>::Tuples mattuples(*spSeq);
 	// @TODO threaded
@@ -254,11 +254,11 @@ DistributedPairwiseRunner::run_batch
 	uint64_t *algn_cnts   = new uint64_t[numThreads + 1];
 	uint64_t nelims_ckthr = 0; // nelims_alnthr = 0, nelims_both = 0;
 
-	int tot_batch_cnt = 0, tot_batch_idx = 0;
-	CHECK_MPI(MPI_Allreduce(&batch_cnt, &tot_batch_cnt, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD));	
-
-	while (tot_batch_idx < tot_batch_cnt)
+	size_t moreToGo = 0, totmoreToGo = 0;
+	do
 	{
+	//	if(moreToGo == 0) goto please;
+
 		uint64_t beg = batch_idx * batch_size;
 		uint64_t end = ((batch_idx + 1) * batch_size > local_nnz_count) ? local_nnz_count : ((batch_idx + 1) * batch_size);
 
@@ -319,7 +319,7 @@ DistributedPairwiseRunner::run_batch
 		nalignments += algn_cnts[numThreads];
 
 		if (algn_cnts[numThreads] == 0)
-		{
+		{	
 			++batch_idx;
 			continue;
 		}
@@ -370,20 +370,22 @@ DistributedPairwiseRunner::run_batch
 
 		// call aligner
 		lfs << "calling aligner for batch idx " << batch_idx
-			<< " cur #algnments " << algn_cnts[numThreads]
-			<< " overall " << nalignments
+			<< " cur #algnments " 		<< algn_cnts[numThreads]
+			<< " overall " 			<< nalignments
 			<< std::endl;
 
 		pf->apply_batch(seqsh, seqsv, lids, col_offset, row_offset, mattuples, lfs, k);
 		
 		delete [] lids;
 
-		if(batch_idx < batch_cnt)
-			++batch_idx;
-		
-		tot_batch_idx = 0;
-		CHECK_MPI(MPI_Allreduce(&batch_idx, &tot_batch_idx, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD));
-	}
+		batch_idx++;
+
+		moreToGo = batch_idx < batch_cnt;
+	
+		CHECK_MPI(MPI_Allreduce(&moreToGo, &totmoreToGo, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD));	
+		//std::cout << "Rank " << myrank << " moreToGo " << moreToGo << " totmoreToGo " << totmoreToGo << std::endl;
+
+	} while(totmoreToGo);
 
 	pf->nalignments = nalignments;
 	pf->print_avg_times(parops, lfs);
