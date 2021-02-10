58978c8fe2bc6a061ea9d7911cdedda992ba9479
Giulia Guidi <gl.guidi93@gmail.com>
2021-02-10 14:28:08 -0800

    update script cori nerscc

--- internal-cori-modules.sh
+++ internal-cori-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-module swap PrgEnv-intel PrgEnv-gnu && module load cmake && module load boost && module load python
-export COMBBLAS_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/
-export BLOOM_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/src/libbloom/
-export SEQAN_HOME=/global/cscratch1/sd/gguidi/IPDPS2021/diBELLA/seqan
+module load cmake && module load boost && module load python
+export COMBBLAS_HOME=$(PWD)
+export BLOOM_HOME=$(PWD)/src/libbloom/
+export SEQAN_HOME=$(PWD)/seqan
