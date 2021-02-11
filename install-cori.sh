44659bf99571dd2e4fcf604bb93d6197249396dc
Giulia Guidi <gl.guidi93@gmail.com>
2021-02-10 16:35:48 -0800

    script installation on cori

---
+++ install-cori.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+module load cmake && module load boost && module load python
+export COMBBLAS_HOME=$PWD
+export BLOOM_HOME=$PWD/src/libbloom/
+export SEQAN_HOME=$PWD/seqan
+
+git clone https://github.com/PASSIONLab/CombBLAS.git
+cd $COMBBLAS_HOME/CombBLAS
+mkdir build
+mkdir install
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install ../
+make -j8
+make install
+
+echo ""
+echo "CombBLAS installation completed."
+echo ""
+
+cd $COMBBLAS_HOME
+mkdir build_release
+cd build_release
+cmake ..
+make -j8
+
+echo "diBELLA 2D installation completed."
+echo ""
