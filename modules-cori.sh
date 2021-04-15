7780f5211a3e912eeaf370d234b69e8ecc4fd4b4
Giulia Guidi <gl.guidi93@gmail.com>
2021-04-14 17:40:40 -0700

    cmake for cuda/logan

--- modules-cori.sh
+++ modules-cori.sh
@@ -2,3 +2,4 @@ module swap PrgEnv-intel PrgEnv-gnu && module load cmake && module load boost &&
 export COMBBLAS_HOME=$PWD
 export BLOOM_HOME=$PWD/src/libbloom/
 export SEQAN_HOME=$PWD/seqan
+export MYPROJDIR=$PWD
