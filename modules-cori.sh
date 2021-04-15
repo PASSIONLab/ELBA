2f3e266502d81145a0f18741b89dbd57d3ef5b87
Giulia Guidi <gl.guidi93@gmail.com>
2021-04-14 17:31:19 -0700

    internal module for cori nersc

---
+++ modules-cori.sh
@@ -0,0 +1,4 @@
+module swap PrgEnv-intel PrgEnv-gnu && module load cmake && module load boost && module load python
+export COMBBLAS_HOME=$PWD
+export BLOOM_HOME=$PWD/src/libbloom/
+export SEQAN_HOME=$PWD/seqan
