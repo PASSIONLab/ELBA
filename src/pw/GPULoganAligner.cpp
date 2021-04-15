e1eb76bd70d03a3756c38e4bd0a6455a9646d13a
Giulia Guidi <gl.guidi93@gmail.com>
2021-04-15 12:59:12 -0700

    working on integrating logan

--- src/pw/GPULoganAligner.cpp
+++ src/pw/GPULoganAligner.cpp
@@ -4,115 +4,115 @@
 
 #include "../../include/pw/GPULoganAligner.hpp"
 
-uint minOverlapLen = 10000;
+// uint minOverlapLen = 10000;
 
-void GPULoganAligner::PostAlignDecision(const AlignmentInfo& ai, bool& passed, float& ratioScoreOverlap, 
-	uint32_t& overhang, uint32_t& overhangT, uint32_t& overlap, const bool noAlign)
-{
-	auto maxseed = ai.seed;	// returns a seqan:Seed object
+// void GPULoganAligner::PostAlignDecision(const AlignmentInfo& ai, bool& passed, float& ratioScoreOverlap, 
+// 	uint32_t& overhang, uint32_t& overhangT, uint32_t& overlap, const bool noAlign)
+// {
+// 	auto maxseed = ai.seed;	// returns a seqan:Seed object
 
-	// {begin/end}Position{V/H}: Returns the begin/end position of the seed in the query (vertical/horizonral direction)
-	// these four return seqan:Tposition objects
-	int begpV = beginPositionV(maxseed);
-	int endpV = endPositionV  (maxseed);
-	int begpH = beginPositionH(maxseed);
-	int endpH = endPositionH  (maxseed);
+// 	// {begin/end}Position{V/H}: Returns the begin/end position of the seed in the query (vertical/horizonral direction)
+// 	// these four return seqan:Tposition objects
+// 	int begpV = beginPositionV(maxseed);
+// 	int endpV = endPositionV  (maxseed);
+// 	int begpH = beginPositionH(maxseed);
+// 	int endpH = endPositionH  (maxseed);
 
-	unsigned short int overlapLenH = ai.seq_h_seed_length;
-	unsigned short int overlapLenV = ai.seq_v_seed_length;
+// 	unsigned short int overlapLenH = ai.seq_h_seed_length;
+// 	unsigned short int overlapLenV = ai.seq_v_seed_length;
 
-	unsigned short int rlenH = ai.seq_h_length;
-	unsigned short int rlenV = ai.seq_v_length;
+// 	unsigned short int rlenH = ai.seq_h_length;
+// 	unsigned short int rlenV = ai.seq_v_length;
 
-	unsigned short int minLeft  = min(begpV, begpH);
-	unsigned short int minRight = min(rlenV - endpV, rlenH - endpH);
+// 	unsigned short int minLeft  = min(begpV, begpH);
+// 	unsigned short int minRight = min(rlenV - endpV, rlenH - endpH);
 
-	overlap = minLeft + minRight + (overlapLenV + overlapLenH) / 2;
+// 	overlap = minLeft + minRight + (overlapLenV + overlapLenH) / 2;
 
-#ifndef FIXEDTHR
-	float myThr = (1 - DELTACHERNOFF) * (ratioScoreOverlap * (float)overlap);
+// #ifndef FIXEDTHR
+// 	float myThr = (1 - DELTACHERNOFF) * (ratioScoreOverlap * (float)overlap);
 
-	// Contained overlaps removed for now, reintroduce them later
-	bool contained = false;
-	bool chimeric  = false; 
+// 	// Contained overlaps removed for now, reintroduce them later
+// 	bool contained = false;
+// 	bool chimeric  = false; 
 
-	// seqH is column entry and seqV is row entry, for each column, we iterate over seqHs
-	int seqH = ai.seq_v_g_idx, seqV = ai.seq_h_g_idx;
+// 	// seqH is column entry and seqV is row entry, for each column, we iterate over seqHs
+// 	int seqH = ai.seq_v_g_idx, seqV = ai.seq_h_g_idx;
 	
-	// Reserve length/position if rc [x]
-	if(ai.rc)
-	{
-		uint tmp = begpV;
-		begpV = rlenV - endpV;
-		endpV = rlenV - tmp;
-	}
+// 	// Reserve length/position if rc [x]
+// 	if(ai.rc)
+// 	{
+// 		uint tmp = begpV;
+// 		begpV = rlenV - endpV;
+// 		endpV = rlenV - tmp;
+// 	}
 
-	if((begpH == 0 & rlenH-endpH == 0) || (begpV == 0 & rlenV-endpV == 0))
-		contained = true;
+// 	if((begpH == 0 & rlenH-endpH == 0) || (begpV == 0 & rlenV-endpV == 0))
+// 		contained = true;
 	
-	if(!contained)
-	{
-		// If noAlign is false, set passed to false if the score isn't good enough
-		if(!noAlign)
-		{
-			if((float)ai.xscore < myThr || overlap < minOverlapLen) passed = false;
-			else passed = true;
-		}
+// 	if(!contained)
+// 	{
+// 		// If noAlign is false, set passed to false if the score isn't good enough
+// 		if(!noAlign)
+// 		{
+// 			if((float)ai.xscore < myThr || overlap < minOverlapLen) passed = false;
+// 			else passed = true;
+// 		}
 
-		if(passed)
-		{
-			uint32_t direction, directionT;
-			uint32_t suffix, suffixT;
+// 		if(passed)
+// 		{
+// 			uint32_t direction, directionT;
+// 			uint32_t suffix, suffixT;
 
-			// !reverse complement
-			if(!ai.rc)
-			{
-				if(begpH > begpV)
-				{
-					direction  = 1;
-					directionT = 2;
+// 			// !reverse complement
+// 			if(!ai.rc)
+// 			{
+// 				if(begpH > begpV)
+// 				{
+// 					direction  = 1;
+// 					directionT = 2;
 
-					suffix  = rlenV - endpV;
-					suffixT = begpH;
-				}	
-				else
-				{
-					direction  = 2;
-					directionT = 1;
+// 					suffix  = rlenV - endpV;
+// 					suffixT = begpH;
+// 				}	
+// 				else
+// 				{
+// 					direction  = 2;
+// 					directionT = 1;
 
-					suffix  = rlenH - endpH;
-					suffixT = begpV;
-				} 
-			}
-			else
-			{
-				if((begpV > 0) & (begpH > 0) & (rlenV-endpV == 0) & (rlenV-endpV == 0))
-				{
-					direction  = 0;
-					directionT = 0;
+// 					suffix  = rlenH - endpH;
+// 					suffixT = begpV;
+// 				} 
+// 			}
+// 			else
+// 			{
+// 				if((begpV > 0) & (begpH > 0) & (rlenV-endpV == 0) & (rlenV-endpV == 0))
+// 				{
+// 					direction  = 0;
+// 					directionT = 0;
 
-					suffix  = begpV; // seqV == 2
-					suffixT = begpH; // seqV == 2			
-				}
-				else
-				{
-					direction  = 3;
-					directionT = 3;
+// 					suffix  = begpV; // seqV == 2
+// 					suffixT = begpH; // seqV == 2			
+// 				}
+// 				else
+// 				{
+// 					direction  = 3;
+// 					directionT = 3;
 
-					suffix  = rlenV - endpV; // seqV == 1, seqH == 2	
-					suffixT = rlenH - endpH; // seqV == 1, seqH == 2		
-				}
-			}
-			overhang  = suffix  << 2 | direction;
-			overhangT = suffixT << 2 | directionT;
-		} // if(passed)
-	} // if(!contained)
+// 					suffix  = rlenV - endpV; // seqV == 1, seqH == 2	
+// 					suffixT = rlenH - endpH; // seqV == 1, seqH == 2		
+// 				}
+// 			}
+// 			overhang  = suffix  << 2 | direction;
+// 			overhangT = suffixT << 2 | directionT;
+// 		} // if(passed)
+// 	} // if(!contained)
 		
-#else
-	if(ai.xscore >= FIXEDTHR)
-		passed = true;
-#endif
-}
+// #else
+// 	if(ai.xscore >= FIXEDTHR)
+// 		passed = true;
+// #endif
+// }
 
 GPULoganAligner::GPULoganAligner(
     ScoringScheme scoring_scheme,
@@ -169,135 +169,161 @@ GPULoganAligner::apply_batch
 	AlignmentInfo *ai = new AlignmentInfo[npairs];
 	std::pair<ushort, ushort> *seedlens = new std::pair<ushort, ushort>[npairs];
 
-	bool *strands = new bool[npairs];
-	int  *xscores = new int[npairs];
-	TSeed  *seeds = new TSeed[npairs];
+	// bool *strands = new bool[npairs]; // Directly in loganResult
+	// int  *xscores = new int[npairs];
+	// TSeed  *seeds = new TSeed[npairs];
+
+	std::vector<string> seqHs;
+	std::vector<string> seqVs;
+	std::vector<SeedL>  seeds;
+	std::vector<loganResult> xscores;
 
 	/* GGGG: seed_count is hardcoded here (2) */
 	for(int count = 0; count < seed_count; ++count)
 	{
 		auto start_time = std::chrono::system_clock::now();
 
-		seqan::StringSet<seqan::Gaps<seqan::Dna5String>> seqsh_ex;
-		seqan::StringSet<seqan::Gaps<seqan::Dna5String>> seqsv_ex;
-		resize(seqsh_ex, npairs, seqan::Exact{});
-		resize(seqsv_ex, npairs, seqan::Exact{});
+		// seqan::StringSet<seqan::Gaps<seqan::Dna5String>> seqsh_ex;
+		// seqan::StringSet<seqan::Gaps<seqan::Dna5String>> seqsv_ex;
+		// resize(seqsh_ex, npairs, seqan::Exact{});
+		// resize(seqsv_ex, npairs, seqan::Exact{});
 	
-    // @GGGG: keep the order for the post alignment evaluation (this might not be feasible in this case? measure slowdown)
-    // #pragma omp parallel for 
-	for(IT j = start; j < end; ++j) // I acculate sequences for GPU batch alignment
-		for (uint64_t i = 0; i < npairs; ++i)
-		{
-			dibella::CommonKmers *cks = std::get<2>(mattuples[lids[i]]);
-
-		#ifdef TWOSEED
-			ushort LocalSeedVOffset =
-				(count == 0) ? cks->first.first : cks->second.first;
-			ushort LocalSeedHOffset =
-				(count == 0) ? cks->first.second : cks->second.second;
-		#else
-			// GGGG: TODO check reverse complement
-			ushort LocalSeedVOffset = cks.pos[0].first;
-			ushort LocalSeedHOffset = cks.pos[0].second;
-		#endif
-
-			seqan::Dna5String seedH;
-			seqan::Dna5String seedV;
-
-			auto start_time = std::chrono::system_clock::now();
-			auto end_time   = std::chrono::system_clock::now();
-
-			// Seed creation params are:
-			// horizontal seed start offset, vertical seed start offset, length
-			TSeed seed(LocalSeedHOffset, LocalSeedVOffset, seed_length);
-
-			seedH = infix(seqan::source(seqsh[i]), beginPositionH(seed), endPositionH(seed));
-			seedV = infix(seqan::source(seqsv[i]), beginPositionV(seed), endPositionV(seed));
-
-			seqan::Dna5StringReverseComplement twin(seedH);
-
-		#ifdef STATS
-			seqan::Align<seqan::Dna5String> align;
-			resize(rows(align), 2);
-		#endif
-
-			if(twin == seedV)
+		// @GGGG: keep the order for the post alignment evaluation (this might not be feasible in this case? measure slowdown)
+		// #pragma omp parallel for 
+		for(IT j = start; j < end; ++j) // I acculate sequences for GPU batch alignment
+			for (uint64_t i = 0; i < npairs; ++i)
 			{
-				strands[i] = true;
-				seqan::Dna5String twinseqH = seqan::source(seqsh[i]);
-				seqan::Dna5StringReverseComplement twinRead(twinseqH);
-				LocalSeedHOffset = length(twinseqH) - LocalSeedHOffset - seed_length;
+				dibella::CommonKmers *cks = std::get<2>(mattuples[lids[i]]);
 
-				setBeginPositionH(seed, LocalSeedHOffset);
-				setBeginPositionV(seed, LocalSeedVOffset);
-				setEndPositionH(seed, LocalSeedHOffset + seed_length);
-				setEndPositionV(seed, LocalSeedVOffset + seed_length);
+			#ifdef TWOSEED
+				ushort LocalSeedVOffset =
+					(count == 0) ? cks->first.first : cks->second.first;
+				ushort LocalSeedHOffset =
+					(count == 0) ? cks->first.second : cks->second.second;
+			#else
+				// GGGG: TODO check reverse complement
+				ushort LocalSeedVOffset = cks.pos[0].first;
+				ushort LocalSeedHOffset = cks.pos[0].second;
+			#endif
 
-				if(!noAlign) 
+				seqan::Dna5String seedH;
+				seqan::Dna5String seedV;
+
+				auto start_time = std::chrono::system_clock::now();
+				auto end_time   = std::chrono::system_clock::now();
+
+				// @GGGG-TODO: remove this stuff and only use string
+				seedH = infix(seqan::source(seqsh[i]), beginPositionH(seed), endPositionH(seed));
+				seedV = infix(seqan::source(seqsv[i]), beginPositionV(seed), endPositionV(seed)); 
+
+				seqan::Dna5StringReverseComplement twin(seedH);
+
+			#ifdef STATS
+				seqan::Align<seqan::Dna5String> align;
+				resize(rows(align), 2);
+			#endif
+
+				std::string seqV = seqsv[i];
+				std::string seqH = seqsh[i];
+
+				logan::loganResult localRes; // not sure we need logan::
+
+				if(twin == seedV)
 				{
-					/* Perform match extension */
-					start_time = std::chrono::system_clock::now();
-					xscores[i] = extendSeed(seed, twinRead, seqan::source(seqsv[i]), seqan::EXTEND_BOTH, scoring_scheme,
-							xdrop, (int)k,
-							seqan::GappedXDrop());
+					localRes.strand = true;
 
-					end_time = std::chrono::system_clock::now();
-					add_time("XA:ExtendSeed", (ms_t(end_time - start_time)).count());
+					std::string twinseqH(seqH);
+
+					// @GGGG-TODO: remove this stuff and only use string
+					seqan::Dna5String twinseqH = seqan::source(seqsh[i]);
+					seqan::Dna5StringReverseComplement twinRead(twinseqH);
+					LocalSeedHOffset = length(twinseqH) - LocalSeedHOffset - seed_length;
+
+					logan::SeedL seed(LocalSeedHOffset, LocalSeedVOffset, LocalSeedHOffset + seed_length, LocalSeedVOffset + seed_length);
+
+					if(!noAlign) 
+					{
+						// GGGG: here only accumulate stuff for the GPUs, don't perform alignment
+						seeds.push_back(seed);
+						seqVs.push_back(seqV);
+						seqHs.push_back(twinseqH);
+
+						xscores.push_back(localRes);
+
+					#ifdef STATS
+						assignSource(row(align, 0), infix(twinRead, beginPositionH(seed),
+														endPositionH(seed)));
+						assignSource(row(align, 1), infix(*seqV, beginPositionV(seed),
+														endPositionV(seed)));
+					#endif
+					}
+					else // @GGGG-TODO: the logic is a bit different than BELLA -- double check and clean unused code
+					{
+						localRes.xscore = 0;
+
+						seeds.push_back(seed);
+						seqVs.push_back(seqV);
+						seqHs.push_back(twinseqH);
+
+						xscores.push_back(localRes);
+					}
 				}
 				else
 				{
-					xscores[i] = 0;
+					localRes.strand = false;
+
+					// @GGGG-TODO: remove this stuff and only use string
+					seqan::Dna5String twinseqH = seqan::source(seqsh[i]);
+					seqan::Dna5StringReverseComplement twinRead(twinseqH);
+					LocalSeedHOffset = length(twinseqH) - LocalSeedHOffset - seed_length;
+
+					logan::SeedL seed(LocalSeedHOffset, LocalSeedVOffset, LocalSeedHOffset + seed_length, LocalSeedVOffset + seed_length);
+
+					if(!noAlign) 
+					{
+						// GGGG: here only accumulate stuff for the GPUs, don't perform alignment
+						seeds.push_back(seed);
+						seqVs.push_back(seqV);
+						seqHs.push_back(twinseqH);
+
+						xscores.push_back(localRes);
+						
+					#ifdef STATS
+						assignSource(row(align, 0), infix(twinRead, beginPositionH(seed),
+														endPositionH(seed)));
+						assignSource(row(align, 1), infix(*seqV, beginPositionV(seed),
+														endPositionV(seed)));
+					#endif
+					}
+					else // @GGGG-TODO: the logic is a bit different than BELLA -- double check and clean unused code
+					{
+						localRes.xscore = 0;
+
+						seeds.push_back(seed);
+						seqVs.push_back(seqV);
+						seqHs.push_back(twinseqH);
+
+						xscores.push_back(localRes);
+					}
 				}
 
 			#ifdef STATS
-				assignSource(row(align, 0), infix(twinRead, beginPositionH(seed),
-												endPositionH(seed)));
-				assignSource(row(align, 1), infix(*seqV, beginPositionV(seed),
-												endPositionV(seed)));
+				xscores[i] = extendSeed(seed, seqan::source(seqsh[i]), seqan::source(seqsv[i]),
+						seqan::EXTEND_BOTH, scoring_scheme,
+						xdrop, (int)k, seqan::GappedXDrop());
+				assignSource(seqsh_ex[i],
+							infix(seqan::source(seqsh[i]),
+								beginPositionH(seed), endPositionH(seed)));
+				assignSource(seqsv_ex[i],
+							infix(seqan::source(seqsv[i]),
+								beginPositionV(seed), endPositionV(seed)));
 			#endif
+				seeds[i] = seed;
+				seedlens[i].first  = static_cast<ushort>(seed._endPositionH -
+														seed._beginPositionH);
+				seedlens[i].second = static_cast<ushort>(seed._endPositionV -
+														seed._beginPositionV);
 			}
-			else
-			{
-				strands[i] = false;
-				if(!noAlign) 
-				{
-					start_time = std::chrono::system_clock::now();
-					xscores[i] = extendSeed(seed, seqan::source(seqsh[i]), seqan::source(seqsv[i]), seqan::EXTEND_BOTH, scoring_scheme,
-							xdrop, (int)k, 
-							seqan::GappedXDrop());
-					end_time = std::chrono::system_clock::now();
-					add_time("XA:ExtendSeed", (ms_t(end_time - start_time)).count());
-
-				#ifdef STATS
-					assignSource(row(align, 0), infix(*seqH, beginPositionH(seed),
-													endPositionH(seed)));
-					assignSource(row(align, 1), infix(*seqV, beginPositionV(seed),
-													endPositionV(seed)));
-				#endif
-				}
-				else
-				{
-					xscores[i] = 0;
-				}
-			}
-
-		#ifdef STATS
-			xscores[i] = extendSeed(seed, seqan::source(seqsh[i]), seqan::source(seqsv[i]),
-					   seqan::EXTEND_BOTH, scoring_scheme,
-					   xdrop, (int)k, seqan::GappedXDrop());
-			assignSource(seqsh_ex[i],
-						 infix(seqan::source(seqsh[i]),
-							   beginPositionH(seed), endPositionH(seed)));
-			assignSource(seqsv_ex[i],
-						 infix(seqan::source(seqsv[i]),
-							   beginPositionV(seed), endPositionV(seed)));
-		#endif
-			seeds[i] = seed;
-			seedlens[i].first  = static_cast<ushort>(seed._endPositionH -
-													 seed._beginPositionH);
-			seedlens[i].second = static_cast<ushort>(seed._endPositionV -
-													 seed._beginPositionV);
-		}
 
 		auto end_time = std::chrono::system_clock::now();
     	add_time("XA:ExtendSeed", (ms_t(end_time - start_time)).count());
@@ -369,6 +395,7 @@ GPULoganAligner::apply_batch
 		end_time = std::chrono::system_clock::now();
     	add_time("XA:ComputeStats", (ms_t(end_time - start_time)).count());
 	}
+
 
 	delete [] seedlens;
 	delete [] xscores;
