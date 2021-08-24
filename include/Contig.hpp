34bd79eaaa95fd55953b0901189130036a652f8e
Giulia Guidi <gl.guidi93@gmail.com>
2021-08-24 11:55:49 -0700

    Commenting out contig gen part for now, working on separate branch

--- include/Contig.hpp
+++ include/Contig.hpp
@@ -16,38 +16,39 @@
 using namespace combblas;
 // typedef ContigSRing <dibella::CommonKmers, dibella::CommonKmers, dibella::CommonKmers> ContigSRing_t;
 
-std::vector<std::string> 
-CreateContig(PSpMat<dibella::CommonKmers>::MPI_DCCols& S, std::string& myoutput, TraceUtils tu, 
-    PSpMat<dibella::CommonKmers>::DCCols* spSeq, std::shared_ptr<DistributedFastaData> dfd, int64_t nreads)
-{    
 
-    float balance = S.LoadImbalance();
-    int64_t nnz   = S.getnnz();
+// std::vector<std::string> 
+// CreateContig(PSpMat<dibella::CommonKmers>::MPI_DCCols& S, std::string& myoutput, TraceUtils tu, 
+//    PSpMat<dibella::CommonKmers>::DCCols* spSeq, std::shared_ptr<DistributedFastaData> dfd, int64_t nreads)
+// {    
 
-    std::ostringstream outs;
-    outs.str("");
-    outs.clear();
-    outs << "CreateContig::LoadBalance: " << balance << endl;
-    outs << "CreateContig::nonzeros: "    << nnz     << endl;
-    SpParHelper::Print(outs.str());
+   // float balance = S.LoadImbalance();
+   // int64_t nnz   = S.getnnz();
 
-    int64_t nCC = 0;
-    FullyDistVec<int64_t, int64_t> myLabelCC = CC(S, nCC);
+   // std::ostringstream outs;
+   // outs.str("");
+   // outs.clear();
+   // outs << "CreateContig::LoadBalance: " << balance << endl;
+   // outs << "CreateContig::nonzeros: "    << nnz     << endl;
+   // SpParHelper::Print(outs.str());
 
-	std::string myccoutput = myoutput + ".cc";
-	myLabelCC.ParallelWrite(myccoutput, 1);
+   // int64_t nCC = 0;
+   // FullyDistVec<int64_t, int64_t> myLabelCC = CC(S, nCC);
 
-    uint64_t nContig = myLabelCC.Reduce(maximum<int64_t>(), (int64_t)0);
-    nContig++; // because of zero based indexing for cluster
+   //	std::string myccoutput = myoutput + ".cc";
+   //	myLabelCC.ParallelWrite(myccoutput, 1);
 
-    std::stringstream ncc;
-    ncc << "nContig: " << nContig << endl;
-    SpParHelper::Print(ncc.str());
+   // uint64_t nContig = myLabelCC.Reduce(maximum<int64_t>(), (int64_t)0);
+   // nContig++; // because of zero based indexing for cluster
 
-    First4Clust(myLabelCC);
-    HistCC(myLabelCC, nCC);
+   // std::stringstream ncc;
+   // ncc << "nContig: " << nContig << endl;
+   // SpParHelper::Print(ncc.str());
 
-    // PrintCC(myLabelCC, nCC);
+   // First4Clust(myLabelCC);
+   // HistCC(myLabelCC, nCC);
+
+   // PrintCC(myLabelCC, nCC);
 
 #ifdef MATRIXPOWER
 
@@ -59,33 +60,33 @@ CreateContig(PSpMat<dibella::CommonKmers>::MPI_DCCols& S, std::string& myoutput,
     // FullyDistVec<int64_t, std::array<char, MAXCONTIGLEN>> ReadVector(S.getcommgrid());
     // char* nt; // NT initial value
 
-    CustomVectorEntry nt;
+    // CustomVectorEntry nt;
     /*
      * A vector of read ids that is my path [0, 1, 3, 67] (paths)
      * A vector of offset [10, 40, 50] (offsets) and offsets.size = paths.size -1 
      * Offset 10 tells me that i have to cut the last 10 bases of read1 and concatenate them to read0, then 40 from read3 and concatenate them to read1 etc. 
     */
-    FullyDistVec<int64_t, char*> ReadVector(S.getcommgrid(), nreads, nt);
-	auto dcsc = spSeq->GetDCSC();
+    // FullyDistVec<int64_t, char*> ReadVector(S.getcommgrid(), nreads, nt);
+    //	auto dcsc = spSeq->GetDCSC();
 
     // GGGG: fill the read vector with sequences
     // IT * ir; //!< row indices, size nz
-	for (uint64_t i = 0; i < dcsc->nz; ++i)
-	{
-		int64_t lrid = dcsc->ir[i]; // local row idx
-        seqan::Dna5String rseq = *(dfd->row_seq(lrid));
+    //	for (uint64_t i = 0; i < dcsc->nz; ++i)
+    //	{
+    //		int64_t lrid = dcsc->ir[i]; // local row idx
+    //    seqan::Dna5String rseq = *(dfd->row_seq(lrid));
 
-        // std::array<char, MAXCONTIGLEN> crseq;
-        std::string cpprseq;
-        std::copy(begin(rseq), end(rseq), begin(cpprseq)); // @GGGG: this doesnt work
+    //    std::array<char, MAXCONTIGLEN> crseq;
+    //    std::string cpprseq;
+    //    std::copy(begin(rseq), end(rseq), begin(cpprseq)); // @GGGG: this doesnt work
 
-        char* crseq = new char[MAXCONTIGLEN];
-        crseq = &cpprseq[0]; // C++14
+    //    char* crseq = new char[MAXCONTIGLEN];
+    //    crseq = &cpprseq[0]; // C++14
 
-        std::cout << rseq << std::endl;
+    //    std::cout << rseq << std::endl;
     
-        ReadVector.SetElement(lrid, crseq); // SetElement only work locally (owner)
-	}
+    //   ReadVector.SetElement(lrid, crseq); // SetElement only work locally (owner)
+    //	}
 
     // ReadVector.DebugPrint();
     // FullyDistVec<int64_t, std::array<char, MAXSEQLEN>> ContigVector(S.getcommgrid());
@@ -105,14 +106,14 @@ CreateContig(PSpMat<dibella::CommonKmers>::MPI_DCCols& S, std::string& myoutput,
 
 #endif
 
-    std::vector<std::string> myContigSet;
+    // std::vector<std::string> myContigSet;
     // @GGGG-TODO: Create contig sequence from connected component matrix
     // {
     //      ...
     // }
 
-    return myContigSet;
-}
+    // return myContigSet;
+// }
 
 // FullyDistVec<int64_t, dibella::CommonKmers> ReduceV(T.getcommgrid());
 // FullyDistVec<int64_t, dibella::CommonKmers> ContigV(T.getcommgrid());
