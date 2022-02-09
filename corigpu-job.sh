ba3b36b6bdaecf6b7ae8abea25b20795aa348f03
Giulia Guidi <gl.guidi93@gmail.com>
2022-02-09 11:39:17 -0800

    scripting

--- corigpu-job.sh
+++ corigpu-job.sh
@@ -11,4 +11,4 @@
 # Despite what its name suggests, --gpus-per-task in the examples below only counts the number of GPUs to allocate to the job; it does not enforce any binding or affinity of GPUs to CPUs or tasks.
 
 export SLURM_CPU_BIND="cores"
-srun -n 16 /global/cscratch1/sd/gguidi/diBELLA.2D/build_release/./dibella -i /global/cscratch1/sd/gguidi/diBELLA.2D/ecoli_hifi.fasta -k 31 --idxmap ecoli.hifi.idxmap -c 191028 --alph dna --af ecoli.hifi.result -s 1 -O 100000 --afreq 100000 --xa 15
+srun -n 16 /global/cscratch1/sd/gguidi/sc-gpu/diBELLA.2D/build_release/./dibella -i /global/cscratch1/sd/gguidi/sc-gpu/diBELLA.2D/ecoli_hifi_29x.fasta -k 31 --idxmap ecoli.hifi.idxmap -c 191028 --alph dna --af ecoli.hifi.result -s 1 -O 100000 --afreq 100000 --xa 15
