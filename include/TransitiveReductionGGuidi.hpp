bab2b5236c38bb60223e657c803938f90139cd8e
Giulia Guidi <gguidi@berkeley.edu>
2022-04-01 19:00:21 -0700

    Update TransitiveReductionGGuidi.hpp

--- include/TransitiveReductionGGuidi.hpp
+++ include/TransitiveReductionGGuidi.hpp
@@ -1,11 +1,136 @@
+#ifndef TRANSITIVE_REDUCTION_H_
+#define TRANSITIVE_REDUCTION_H_
+
+#include "../include/ReadOverlap.hpp"
+
+#include <sys/time.h>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct InvalidSRing : unary_function<ReadOverlap, ReadOverlap>
+{
+    bool operator() (const ReadOverlap& x) { return x.is_invalid(); }
+};
+
+struct TransposeSRing : unary_function <ReadOverlap, ReadOverlap>
+{
+    ReadOverlap operator() (const ReadOverlap& x) const
+    {
+        ReadOverlap xT = x;
+
+        xT.b[0] = x.l[1] - x.e[1];
+        xT.e[0] = x.l[1] - x.b[1];
+        xT.b[1] = x.l[0] - x.e[0];
+        xT.e[1] = x.l[0] - x.b[0];
+
+        xT.l[0] = x.l[1];
+        xT.l[1] = x.l[0];
+
+        xT.rc = x.rc;
+        xT.transpose = !x.transpose;
+
+        xT.sfx  = x.sfxT;
+        xT.sfxT = x.sfx;
+        xT.dir  = x.dirT;
+        xT.dirT = x.dir;
+
+        return xT;
+    }
+};
+
+struct PlusFuzzSRing : unary_function<ReadOverlap, ReadOverlap>
+{
+    ReadOverlap operator() (ReadOverlap& x) const
+    {
+        ReadOverlap fuzzed = x;
+        fuzzed.sfx += FUZZ;
+        fuzzed.sfxT += FUZZ;
+
+        return fuzzed;
+    }
+};
+
+struct TransitiveSelection : binary_function<ReadOverlap, OverlapPath, bool>
+{
+    bool operator() (const ReadOverlap& r, const OverlapPath& n) const
+    {
+        int dir = r.dir;
+
+        if (dir == -1) return false;
+
+        return (r.sfx >= n.sfx[dir]);
+    }
+};
+
+struct TransitiveRemoval : binary_function<ReadOverlap, bool, ReadOverlap>
+{
+    ReadOverlap operator() (ReadOverlap& x, const bool& y)
+    {
+        if (!y) x.dir = -1;
+        return x;
+    }
+};
+
+struct ZeroPrune { bool operator() (const bool& x) { return false; } };
+struct BoolPrune { bool operator() (const bool& x) { return !x; } };
+
+OverlapPath opmin(const OverlapPath& e1, const OverlapPath& e2)
+{
+    OverlapPath e = OverlapPath();
+
+    for (int i = 0; i < 4; ++i)
+        e.sfx[i] = std::min(e1.sfx[i], e2.sfx[i]);
+
+    return e;
+}
+
+struct MinPlusSR
+{
+    static OverlapPath id() { return OverlapPath(); }
+    static bool returnedSAID() { return false; }
+    static MPI_Op mpi_op() { return MPI_MIN; }
+
+    static OverlapPath add(const OverlapPath& e1, const OverlapPath& e2)
+    {
+        return opmin(e1, e2);
+    }
+
+    static OverlapPath multiply(const ReadOverlap& e1, const ReadOverlap& e2)
+    {
+        OverlapPath e = OverlapPath();
+
+        int t1, t2, h1, h2;
+
+        if (!e1.arrows(t1, h1) || !e2.arrows(t2, h2))
+            return e;
+
+        if (t2 == h1)
+            return e;
+
+        e.sfx[2*t1 + h2] = e1.sfx + e2.sfx;
+
+        return e;
+    }
+
+    static void axpy(ReadOverlap a, const ReadOverlap& x, OverlapPath& y)
+    {
+        y = opmin(y, multiply(a, x));
+    }
+};
+
 /* TR main function */
 #define MAXITER 15
 
-// void TransitiveReduction(SpParMat<int64_t, ReadOverlap, SpDCCols<int64_t, ReadOverlap>>& R, TraceUtils tu)
 void TransitiveReductionOld(PSpMat<ReadOverlap>::MPI_DCCols& B, TraceUtils tu)
 {
 
-    // SpParMat<int64_t, ReadOverlap, SpDCCols<int64_t, ReadOverlap>> RT = R; 
     PSpMat<ReadOverlap>::MPI_DCCols RT = B; /* copies everything */
     RT.Transpose();
     RT.Apply(TransposeSRing()); /* flips all the coordinates */
@@ -31,11 +156,9 @@ void TransitiveReductionOld(PSpMat<ReadOverlap>::MPI_DCCols& B, TraceUtils tu)
     R.PrintInfo();
 
     uint nnz, prev;
-    // uint check_phase_counter = 0;  // GGGG: I don't fully understand this
-    // uint full_counter = 0;         // GGGG: I don't fully understand this
 
-    uint count = 0;     // GGGG: I don't fully understand this
-    uint countidle = 0; // GGGG: I don't fully understand this
+    uint count = 0;     
+    uint countidle = 0; 
 
     /* Gonna iterate on T until there are no more transitive edges to remove */
     do
@@ -79,9 +202,6 @@ void TransitiveReductionOld(PSpMat<ReadOverlap>::MPI_DCCols& B, TraceUtils tu)
             */
         I.EWiseMult(IT, false); // GGGG: this operation doesn't make sense to me, missing something? (see comment right above)
 
-        /* GGGG: you don't need a copy for T, it can be done in place */
-        // SpParMat<int64_t, bool, SpDCCols<int64_t, bool>> Tc = T;
-
         bool isLogicalNot = true;
         bool bId = false;
 
@@ -89,7 +209,7 @@ void TransitiveReductionOld(PSpMat<ReadOverlap>::MPI_DCCols& B, TraceUtils tu)
            If T(i,j) = 1 and I(i,j) = 1 why the resulting T(i,j) should be 0? Don't we wanna remove that?
            this should be an OR, not a NAND: return (x || y);
         */
-        T = EWiseApply<bool, SpDCCols<int64_t, bool>>(T, I, [](bool x, bool y) { return !(x && y); }, isLogicalNot, bId); 
+        T = EWiseApply<bool, SpDCCols<int64_t, bool>>(T, I, [](bool x, bool y) { return (x || y); }, isLogicalNot, bId); 
         cur = T.getnnz();
 
         /* GGGG:
@@ -139,3 +259,5 @@ void TransitiveReductionOld(PSpMat<ReadOverlap>::MPI_DCCols& B, TraceUtils tu)
     tu.print_str("Matrix S, i.e. AAt post transitive reduction: ");
     R.PrintInfo();
 }
+
+#endif
