c475723e3552752d7aad715a51abf8a7a8145500
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-04-17 19:00:41 -0700

    updated handler

--- include/ReadOverlap.hpp
+++ include/ReadOverlap.hpp
@@ -190,7 +190,7 @@ struct ReadOverlapCoordsHandler
     template <typename c, typename t>
     void save(std::basic_ostream<c,t>& os, const ReadOverlap& e, int64_t row, int64_t col)
     {
-        os << static_cast<int>(e.rc) << "\t" << e.b[0] << "\t" << e.e[0] << "\t" << e.b[1] << "\t" << e.e[1] << "\t" << e.l[0] << "\t" << e.l[1];
+        os << static_cast<int>(e.rc) << "\t" << e.dir << "\t" << e.dirT << "\t" << e.sfx << "\t" << e.sfxT << "\t" << e.b[0] << "\t" << e.e[0] << "\t" << e.b[1] << "\t" << e.e[1] << "\t" << e.l[0] << "\t" << e.l[1];
     }
 };
 
