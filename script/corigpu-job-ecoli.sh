5cdc4c42549da1f7e4054b2313e91557debce8ee
Giulia Guidi <gguidi@berkeley.edu>
2022-10-05 13:12:00 -0700

    renaming executable in script

--- script/corigpu-job-ecoli.sh
+++ script/corigpu-job-ecoli.sh
@@ -19,4 +19,4 @@ export OMP_PROC_BIND=spread
 # Despite what its name suggests, --gpus-per-task in the examples below only counts the number of GPUs to allocate to the job; it does not enforce any binding or affinity of GPUs to CPUs or tasks.
 
 export SLURM_CPU_BIND="cores"
-srun -n 16 /global/cscratch1/sd/gguidi/diBELLA.2D/build_release/./dibella -i /global/cscratch1/sd/gguidi/diBELLA.2D/ecoli_hifi_29x.fasta -k 31 --idxmap ecoli.hifi.29x.idxmap -c 17210 --alph dna --af ecoli.hifi.29x.result -s 1 -O 100000 --afreq 100000 --xa 15
+srun -n 16 /global/cscratch1/sd/gguidi/diBELLA.2D/build_release/./elba -i /global/cscratch1/sd/gguidi/diBELLA.2D/ecoli_hifi_29x.fasta -k 31 --idxmap ecoli.hifi.29x.idxmap -c 17210 --alph dna --af ecoli.hifi.29x.result -s 1 -O 100000 --afreq 100000 --xa 15
