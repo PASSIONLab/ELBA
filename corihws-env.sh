b7ef27708bc879c971d1d5fb3534ef203ef26d30
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-04-26 09:39:09 -0700

    No more Cori. Also, I want to replace Cmake with Makefile. In my tests so far on perlmutter, I haven't had to load PrgEnv-gnu, and CombBLAS will be handled by Makefile. Seqan and libbloom will be integrated directly.

--- corihws-env.sh
+++
@@ -1,9 +0,0 @@
-module swap PrgEnv-intel PrgEnv-gnu
-module load cmake
-module load boost
-module load python
-
-export COMBBLAS_HOME=$PWD
-export BLOOM_HOME=$PWD/src/libbloom/
-export SEQAN_HOME=$PWD/seqan
-export MYPROJDIR=$PWD
--- seqan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone --branch seqan-v2.4.0 https://github.com/seqan/seqan.git
-export SEQAN_HOME=$(pwd)/seqan/
-cd $SEQAN_HOME
-git checkout develop
