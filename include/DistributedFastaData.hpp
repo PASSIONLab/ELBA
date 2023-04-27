274b6650f77cdfb353327ec211a66237e9620967
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-04-27 15:54:10 -0700

    smaller neighbor

--- include/DistributedFastaData.hpp
+++ include/DistributedFastaData.hpp
@@ -7,38 +7,8 @@
 class DistributedFastaData
 {
 public:
-
-    struct NbrData
-    {
-        size_t startid;
-        size_t numseqs;
-        int source;
-        int rc_flag;
-
-        NbrData(size_t startid, size_t numseqs, int source, int rc_flag) : startid(startid), numseqs(numseqs), source(source), rc_flag(rc_flag) {}
-
-        // static size_t findnbrs(std::vector<NbrData>& neighbors, const std::vector<size_t>& idoffsets, size_t startid, size_t totreads, int rc_flag, Grid commgrid)
-        // {
-            // int myrank = commgrid->GetRank();
-            // size_t numadded = 0;
-
-            // auto iditr = std::upper_bound(idoffsets.cbegin(), idoffsets.cend(), startid);
-            // iditr--;
-
-            // assert(*iditr <= startid);
-
-            // while (*iditr < startid + totreads)
-            // {
-                // int reqrank = iditr - idoffsets.cbegin();
-                // size_t reqstart = std::max(*iditr++, startid);
-                // size_t reqend = std::min(*iditr, startid + totreads);
-                // neighbors.emplace_back(reqstart, reqend-reqstart, reqrank, rc_flag);
-                // numadded++;
-            // }
-
-            // return numadded;
-        // }
-    };
+    struct NbrData { uint16_t id : 15, rc_flag : 1; };
+    static_assert(sizeof(NbrData) == 2);
 
     DistributedFastaData(FIndex index);
     ~DistributedFastaData();
