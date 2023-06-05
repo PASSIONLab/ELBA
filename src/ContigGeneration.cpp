f9b45ad957002eab592d044e62a47eb1784ef8e4
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-06-05 11:59:56 -0700

    looks right, but definitely need to check and comment last changes

--- src/ContigGeneration.cpp
+++ src/ContigGeneration.cpp
@@ -53,12 +53,57 @@ int64_t GetRead2Contigs(CT<Overlap>::PSpParMat& S, CT<int64_t>::PDistVec& assign
     return numcontigs;
 }
 
+CT<int64_t>::PDistVec GetContigSizes(const CT<int64_t>::PDistVec& assignments, int64_t numcontigs, DistributedFastaData& dfd)
+{
+    auto commgrid = assignments.getcommgrid();
+    int myrank = commgrid->GetRank();
+    int nprocs = commgrid->GetSize();
+    MPI_Comm comm = commgrid->GetWorld();
+
+    std::vector<int64_t> local_component_sizes(numcontigs, 0);
+    std::vector<int64_t> local_components = assignments.GetLocVec();
+
+    int64_t local_size = local_components.size();
+
+    for (int64_t i = 0; i < local_size; ++i)
+    {
+        ++local_component_sizes[local_components[i]];
+    }
+
+    int avgsize = numcontigs / nprocs;
+    int lastsize = numcontigs - (avgsize * (nprocs - 1));
+
+    std::vector<int> recvcounts(nprocs, avgsize);
+    recvcounts.back() = lastsize;
+
+    int mysize = (myrank != nprocs-1)? avgsize : lastsize;
+
+    std::vector<int64_t> fill_vec_components(mysize);
+
+    MPI_Reduce_scatter(local_component_sizes.data(), fill_vec_components.data(), recvcounts.data(), MPI_INT64_T, MPI_SUM, comm);
+
+    return CT<int64_t>::PDistVec(fill_vec_components, commgrid);
+}
+
 std::vector<std::string> GenerateContigs(CT<Overlap>::PSpParMat& S, const DnaBuffer& mydna, DistributedFastaData& dfd)
 {
     std::vector<std::string> contigs;
 
+    /*
+     * Compute a mapping of read IDs to contig IDs. The function @GetRead2Contigs
+     * is responsible for computing the contig sets from the string graph.
+     */
     CT<int64_t>::PDistVec assignments(S.getcommgrid());
+    int64_t numcontigs = GetRead2Contigs(S, assignments);
 
-    GetRead2Contigs(S, assignments);
+    assignments.DebugPrint();
+
+    /*
+     * Compute contig sizes.
+     */
+    CT<int64_t>::PDistVec contigsizes = GetContigSizes(assignments, numcontigs, dfd);
+
+    contigsizes.DebugPrint();
+
     return contigs;
 }
