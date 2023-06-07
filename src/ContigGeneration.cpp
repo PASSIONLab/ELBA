e780e1269a10b37cabab815d863093ae2ef5a658
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-06-07 16:37:49 -0700

    compute sorted sizes

--- src/ContigGeneration.cpp
+++ src/ContigGeneration.cpp
@@ -1,5 +1,6 @@
 #include "ContigGeneration.hpp"
 #include "CC.hpp"
+#include "Logger.hpp"
 
 /*
  * @func GetRead2Contigs     determines which reads belong to which contigs.
@@ -53,22 +54,37 @@ int64_t GetRead2Contigs(CT<Overlap>::PSpParMat& S, CT<int64_t>::PDistVec& assign
     return numcontigs;
 }
 
-CT<int64_t>::PDistVec GetContigSizes(const CT<int64_t>::PDistVec& assignments, int64_t numcontigs, DistributedFastaData& dfd)
+std::vector<std::tuple<int64_t, int64_t>> GetContigSizes(const CT<int64_t>::PDistVec& assignments, int64_t numcontigs, DistributedFastaData& dfd)
 {
     auto commgrid = assignments.getcommgrid();
     int myrank = commgrid->GetRank();
     int nprocs = commgrid->GetSize();
     MPI_Comm comm = commgrid->GetWorld();
 
+    /*
+     * @local_component_sizes is a local vector measuring the number of reads (stored locally)
+     * on each contig. Its range is across the entire global set of contigs, but only counts
+     * the contributors from my processor.
+     */
     std::vector<int64_t> local_component_sizes(numcontigs, 0);
     std::vector<int64_t> local_components = assignments.GetLocVec();
 
     int64_t local_size = local_components.size();
 
+    /*
+     * @local_components[i] is the contig ID of the ith local read. Find
+     * its bucket and increment its counter (which is initialized to zero).
+     */
     for (int64_t i = 0; i < local_size; ++i)
     {
         ++local_component_sizes[local_components[i]];
     }
+
+    /*
+     * Compute and distribute the global contig sizes (number of reads per contig) using
+     * a reduce-scatter collective. The results are distributed linearly to each processor.
+     * The local partition of contig sizes is stored in @locsizes.
+     */
 
     int avgsize = numcontigs / nprocs;
     int lastsize = numcontigs - (avgsize * (nprocs - 1));
@@ -76,40 +92,33 @@ CT<int64_t>::PDistVec GetContigSizes(const CT<int64_t>::PDistVec& assignments, i
     std::vector<int> recvcounts(nprocs, avgsize);
     recvcounts.back() = lastsize;
 
-    int mysize = (myrank != nprocs-1)? avgsize : lastsize;
+    int64_t mysize = (myrank != nprocs-1)? avgsize : lastsize;
 
-    std::vector<int64_t> fill_vec_components(mysize);
+    std::vector<int64_t> locsizes(mysize);
 
-    MPI_Reduce_scatter(local_component_sizes.data(), fill_vec_components.data(), recvcounts.data(), MPI_INT64_T, MPI_SUM, comm);
+    MPI_Reduce_scatter(local_component_sizes.data(), locsizes.data(), recvcounts.data(), MPI_INT64_T, MPI_SUM, comm);
 
-    return CT<int64_t>::PDistVec(fill_vec_components, commgrid);
-}
+    int64_t lengthuntil;
+    MPI_Exscan(&mysize, &lengthuntil, 1, MPI_INT64_T, MPI_SUM, comm);
+    if (myrank == 0) lengthuntil = 0;
 
-std::vector<std::tuple<int64_t, int64_t>> GetAllContigSizesSorted(CT<int64_t>::PDistVec& contigsizes, int64_t& num_used_contigs, DistributedFastaData& dfd)
-{
-    auto commgrid = contigsizes.getcommgrid();
-    int myrank = commgrid->GetRank();
-    int nprocs = commgrid->GetSize();
-    MPI_Comm comm = commgrid->GetWorld();
-
+    /*
+     * Construct vector of tuples whose entries are (global contig ID, contig size),
+     * where we only store contigs that have at least 2 reads.
+     */
     std::vector<std::tuple<int64_t, int64_t>> sendbuf;
-    std::vector<int64_t> locsizes = contigsizes.GetLocVec();
-    int64_t lengthuntil = contigsizes.LengthUntil();
-
-    for (int64_t i = 0; i < locsizes.size(); ++i)
+    for (int64_t i = 0; i < mysize; ++i)
         if (locsizes[i] >= 2)
             sendbuf.emplace_back(i + lengthuntil, locsizes[i]);
 
-    std::vector<int> recvcounts(nprocs);
     std::vector<int> displs(nprocs);
 
+    std::fill(recvcounts.begin(), recvcounts.end(), static_cast<int64_t>(0));
     recvcounts[myrank] = sendbuf.size();
-
-    MPI_Allgather(MPI_IN_PLACE, 0, MPI_INT, recvcounts.data(), 1, MPI_INT, comm);
-
+    MPI_Allgather(MPI_IN_PLACE, 1, MPI_INT, recvcounts.data(), 1, MPI_INT, comm);
     std::exclusive_scan(recvcounts.begin(), recvcounts.end(), displs.begin(), static_cast<int64_t>(0));
 
-    num_used_contigs = recvcounts.back() + displs.back();
+    int64_t num_used_contigs = recvcounts.back() + displs.back();
 
     std::vector<std::tuple<int64_t, int64_t>> result(num_used_contigs);
 
@@ -134,23 +143,7 @@ std::vector<std::string> GenerateContigs(CT<Overlap>::PSpParMat& S, const DnaBuf
     /*
      * Compute contig sizes.
      */
-    CT<int64_t>::PDistVec contigsizes = GetContigSizes(assignments, numcontigs, dfd);
-
-    int64_t num_used_contigs;
-
-    std::vector<std::tuple<int64_t, int64_t>> AllContigSizesSorted = GetAllContigSizesSorted(contigsizes, num_used_contigs, dfd);
-
-    if (S.getcommgrid()->GetRank() == 0)
-    {
-        for (auto& item : AllContigSizesSorted)
-        {
-            std::cout << std::get<0>(item) << "\t" << std::get<1>(item) << "\n";
-        }
-
-        std::cout << std::endl;
-    }
-
-    MPI_Barrier(S.getcommgrid()->GetWorld());
+    auto contigsizes = GetContigSizes(assignments, numcontigs, dfd);
 
     return contigs;
 }
